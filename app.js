534569ff330544e5cf954fde465fb2dbdc8cedef
Jamie King <king0120@gmail.com>
2015-12-11 15:42:57 -0500

    It works ;D

--- app.js
+++ app.js
@@ -75,40 +75,12 @@ $(document).ready(function(){
   function checkWin(player){
     switch (true){
       case $('.box1').hasClass(player) && $('.box2').hasClass(player) && $('.box3').hasClass(player):
-        winMessage();
-        counter = 0;
-        newGame();
-        break;
       case $('.box4').hasClass(player) && $('.box5').hasClass(player) && $('.box6').hasClass(player):
-        winMessage();
-        counter = 0;
-        newGame();
-        break;
       case $('.box7').hasClass(player) && $('.box8').hasClass(player) && $('.box9').hasClass(player):
-        winMessage();
-        counter = 0;
-        newGame();
-        break;
       case $('.box1').hasClass(player) && $('.box4').hasClass(player) && $('.box7').hasClass(player):
-        winMessage();
-        counter = 0;
-        newGame();
-        break;
       case $('.box2').hasClass(player) && $('.box5').hasClass(player) && $('.box8').hasClass(player):
-        winMessage();
-        counter = 0;
-        newGame();
-        break;
       case $('.box3').hasClass(player) && $('.box6').hasClass(player) && $('.box9').hasClass(player):
-        winMessage();
-        counter = 0;
-        newGame();
-        break;
       case $('.box1').hasClass(player) && $('.box5').hasClass(player) && $('.box9').hasClass(player):
-        winMessage();
-        counter = 0;
-        newGame();
-        break;
       case $('.box3').hasClass(player) && $('.box5').hasClass(player) && $('.box7').hasClass(player):
         winMessage();
         counter = 0;
